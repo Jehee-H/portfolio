e596da1817ea9dfeb38100fcd429f5b9e918e7d8
jehee han <jehee.han@iem.thm.de>
2025-06-25 04:02:14 +0200

    fck this value never read error

--- src/pages/Home copy.tsx
+++ src/pages/Home copy.tsx
@@ -1,9 +1,9 @@
-import { motion } from "framer-motion";
+
 // import bgImg from "../assets/placeholder.jpg";
 // import pbImg from "../assets/me.png";
 import "../styles/home.css";
 import HomepageContent from "../components/HomepageContent.tsx";
-import background from "../assets/background.mp4";
+
 
 const Home = () => {
   return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+
 // import bgImg from "../assets/placeholder.jpg";
 // import pbImg from "../assets/me.png";
 import "../styles/home.css";
